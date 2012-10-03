aa76d966f56b04975c7b4846f243a08b99f4a09d
Raul Baldner Junior <raul_baldner_junior@hotmail.com>
2012-10-03 14:54:39 -0300

    Added required autoloads to support CRUD funcionality

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('twig');
+$autoload['libraries'] = array('twig', 'table');
 
 
 /*
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('twig');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url');
+$autoload['helper'] = array('html', 'url');
 
 
 /*
