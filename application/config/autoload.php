50143f5bf1bd1c03c1e5255514a10f0707d101f8
Raul Baldner Junior <raul_baldner_junior@hotmail.com>
2012-10-09 13:54:41 -0300

    Added autoloads

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('twig', 'table');
+$autoload['libraries'] = array('twig', 'table', 'session', 'ion_auth');
 
 
 /*
@@ -96,7 +96,7 @@ $autoload['config'] = array();
 |
 */
 
-$autoload['language'] = array();
+$autoload['language'] = array('ion_auth');
 
 
 /*
